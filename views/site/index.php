87999a8f4837ffe8fab9b2e585f46151a3eee770
Zohid <zohid.valiev@gmail.com>
2020-10-05 11:14:54 +0500

    First Commit

---
+++ views/site/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zohid
+ * Date: 01.10.2020
+ * Time: 16:29
+ */
+
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+        </div>
+    </div>
+</div>
